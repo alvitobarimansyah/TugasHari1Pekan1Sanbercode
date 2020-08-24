3ae05c3a96084d773b645fe7b5aa6b39f77ea27b
Alvito Barimansyah <55906705+alvitobarimansyah@users.noreply.github.com>
2020-08-24 21:29:41 +0700

    submit tugas

---
+++ TugasHari1.py
@@ -0,0 +1,44 @@
+# soal no 1
+
+print("Hello World Python")
+
+# soal no 2
+
+x = int(input("Masukkan Nilai : "))
+y = int(input("Masukkan Nilai : "))
+
+# penjumlahan
+print("Hasil Penjumlahan dari x + y = ", x + y)
+
+# pengurangan
+print("Hasil Pengurangan dari x - y = ", x - y)
+
+# perkalian
+print("Hasil Perkalian dari x * y = ", x * y)
+
+# pembagian
+print("Hasil Pembagian dari x / y = ", int(x / y))
+
+# soal no 3
+
+a = int(input("Masukkan Nilai : "))
+b = int(input("Masukkan Nilai : "))
+
+# hasil pembagian a dan b bertipe data integer
+print("Hasil Pembagian a dan b bertipe data integer : ", int(a / b))
+
+# hasil pembagian a dan b bertipe data float
+print("Hasil Pembagian a dan b bertipe data float : ", a / b)
+
+# soal no 4
+
+firstname = "Alvito"
+lastname = "Barimansyah"
+
+print("Hello sanbercode, saya " + firstname + " " + lastname + "! saya siap belajar python data science.")
+
+# soal no 5
+
+p = 9.99999
+q = 'the number: '
+print(q + str(p))
